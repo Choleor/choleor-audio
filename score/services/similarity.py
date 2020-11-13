1e84e708423faf04b69247f12f1b59248da4150f
HyelinSung <59791809+hlezg08@users.noreply.github.com>
2020-11-13 23:42:10 +0900

    refactoring similarity

--- score/services/similarity.py
+++ score/services/similarity.py
@@ -9,24 +9,16 @@ import librosa.display,librosa
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import cosine_similarity
-import time #코드 동작 소요시간 확인할 때
 
 def getFileList(filePath):
     fileList = []
     for file in os.listdir(filePath):
-        if(audio_slice_id==file):
-            fileList.append(file)
+        fileList.append(file)
     return fileList
-
-"""
-소요 시간 확인
-start = time.time() 
-print("time :", time.time() - start)
-"""
 
 # Extract Music Features: MFCC
 # 처음에 training할 파일들의 feat 추출
-def getFeature(filePath, fileList):
+def getFeatureAll(filePath, fileList):
     X = pd.DataFrame()
     columns = []
     for i in range(20):
@@ -100,7 +92,8 @@ def check_perform_kmeans(data):
 def cos_sim(A, B):
     return dot(A, B) / (norm(A) * norm(B))
 
-def cluster_sim(labels,data,idx,num):
+def cluster_sim(data,idx,n):
+    # idx:유사도 비교를 원하는 파일의 인덱스 번호,n:유사한 파일을 내림차순으로 상위 n개 보여줌
     sim_dic = {}
 
     for i in range(len(data.values)):
@@ -114,46 +107,57 @@ def cluster_sim(labels,data,idx,num):
         sim = sim * 100  # 퍼센트(%) 단위로 나타냄
         sim = round(sim, 2)  # 소수 둘째자리에서 반올림
 
-        audio_slice_id = labels['fileName'].values[i]
+        audio_slice_id = data.index[i]
 
         sim_dic[audio_slice_id] = sim
 
     final_dic = sorted(sim_dic.items(), reverse=True, key=lambda x: x[1])  # 내림차순 정렬
 
-    return final_dic[1:num]
+    return final_dic[1:n]
 
-if __name__ == '__main__':
-    # ""안에 파일 디렉토리 입력
-    fileList = getFileList("C:/")
-    data = getFeature("C:/", fileList)
-    # Convert DataFrame to CSV
-    data.to_csv("music_feature.csv", mode='w')
+def similiarity(file):# 유사도 비교를 원하는 파일명(file)
+    """
+    # DB에 있는 모든 노래 feature구할 때 한번만 돌리기
+    fileList = getFileList("C:/") #parameter:노래 디렉토리
+    data = getFeatureAll("C:/", fileList)  #parameter:노래 디렉토리
 
-    # 여기서부터 유저가 선택한 노래 들어올 때 실행
+    data.to_csv("music_feature.csv", mode='w') # Convert DataFrame to CSV
+    """
+
+    # 여기서부터 유저가 선택한 노래의 구간이 들어올 때 실행
     # Convert DataFrame to CSV and append single file
-    new_data = getFeatureFile("C:/", "유저 파일이름")
+    new_data = getFeatureFile("C:/ffmpeg/bin/input/", file)
     new_data.to_csv('music_feature.csv', mode='a', header=False)
 
     # Load music_feature.csv file
     feat_data = pd.read_csv("music_feature.csv", index_col=[0])
 
-    # KMeans Clustering
+    # KMeans Clustering(n_clusters가 군집 개수)
     n_clusters = 10
     model = KMeans(n_clusters)
     model.fit(feat_data)
-    data_labels = model.predict(feat_data)
+    labels_data = model.predict(feat_data)
+    #결과 값: [0,1,1,2,5,...]와 같은 형태로 나옴
 
-    # cluster labeling 시각화
-    fileList.append("유저 파일이름")
-    print(len(data_labels), len(fileList))
-    kmeans_labels = pd.DataFrame({"fileName": fileList, "Labels": data_labels})
-    # cluster label 번호별로 필터링 하여 시각화
-    filtering = (kmeans_labels['Labels'] == 1) #숫자 바꿔주기
-    filter_kmeans_labels = kmeans_labels[filtering]
+    cluster_idx = [] #같은 군집으로 모여있는 파일의 인덱스를 모아놓은 리스트
+    for i in range(len(labels_data)):
+        # 새로 들어온 파일의 군집 번호와 일치할 때 cluster_idx로 append
+        if (labels_data[i] == labels_data[len(feat_data) - 1]):
+            cluster_idx.append(i)
 
-    # cluster label 번호별로 music feature csv파일 필터링
-    filter_feat = feat_data.iloc[
-        filter_kmeans_labels.index[filter_kmeans_labels['fileName'] == filter_kmeans_labels['fileName'].values[0:]]
-    ]
+    # 같은 군집으로 모여있는 파일의 feature만 필터링
+    filter_feat_data = feat_data.iloc[cluster_idx]
 
-    print(cluster_sim(filter_kmeans_labels,filter_feat,0, 6))
+    for i in range(len(filter_feat_data)):
+        # 유사도 비교를 원하는 파일의 인덱스 번호 찾기(cluster_sim 함수 파라미터에 넣기 위해)
+        if (filter_feat_data.index[i] == file):
+            idx = i
+
+    # 유사도 비교 함수 실행, 결과는 딕셔너리 타입
+    # idx:유사도 비교를 원하는 파일의 인덱스 번호,상위 6개의 유사한 노래 보여줌
+    print(cluster_sim(filter_feat_data, idx, 6))
+    #결과 예시: [('4-TbQnONe_w_16.wav', 95.48), ('4-TbQnONe_w_19.wav', 95.42), ('4-TbQnONe_w_17.wav', 95.27),...]
+
+if __name__ == '__main__':
+    # 따옴표 안에 원하는 노래 파일명 삽입
+    similiarity("Jason Derulo - Kiss The Sky(0).wav")
